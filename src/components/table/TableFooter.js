94b45bd3a1af0d81216af465432fda3f359f18b4
Sean K. Stewart <seankstewart@gmail.com>
2022-09-03 14:59:08 -0400

    add manual updates rates button

--- src/components/table/TableFooter.js
+++ src/components/table/TableFooter.js
@@ -2,13 +2,17 @@ import React from 'react'
 import SaveButton from '../buttons/SaveButton'
 import DeleteButton from '../buttons/DeleteButton'
 import AddButton from '../buttons/AddButton'
+import UpdateRatesButton from '../buttons/UpdateRatesButton'
 
 const TableFooter = (props) => {
   return (
     <tfoot>
         <tr>
             <td colSpan="7" align="left">
-                <AddButton>Add</AddButton>
+                <div style={{display:'flex',justifyContent:'flex-start'}}>
+                  <AddButton>Add</AddButton>
+                  <UpdateRatesButton>Update Rates</UpdateRatesButton>
+                </div>
                 <div style={{display:'flex',justifyContent:'flex-end'}}>
                   <SaveButton>Save</SaveButton>
                   <DeleteButton>Delete</DeleteButton>
